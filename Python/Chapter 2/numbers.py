c4b36ce4683b66170c6714d686c6bc5b3e5068de
umerhassan26 <umerhassan2694@gmail.com>
2024-05-14 16:26:25 +0500

    Lists Python

---
+++ Python/Chapter 2/numbers.py
@@ -0,0 +1,10 @@
+print(2+3)
+print (2**3)
+
+# floats
+print(2.3*2.4)
+print (4/2)
+print (4*6)
+print (5/3)
+a ,b , c = 1 ,2.2,3
+print(a,b,c)
---
+++ Python/Chapter 3/lists.py
@@ -0,0 +1,8 @@
+students = ['ahmad','osama','ashad',23,34,23]
+print(students)
+#Access Elements
+print(students[0].title())
+#f string example
+
+message = f"My student name is {students[1].title()}"
+print(message)

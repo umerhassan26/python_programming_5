f6c5c9daabd2cffedd5b913c44ba4cc1e4f2c0e2
umerhassan26 <umerhassan2694@gmail.com>
2024-05-29 17:02:15 +0500

    Django Templates , Pages App

--- Django/pagesapp/django_project/settings.py
+++ Django/pagesapp/django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)53=8(=a^hvryyq&vd4gn*8^=(#(^u5od(&)gy4elhfolv1mqv"
+SECRET_KEY = "django-insecure-je2kq@z3_vt@)hbgai1-^peim4k@zn3hx^1ss098mk&*85j$h3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
- "django.contrib.admin",
- "django.contrib.auth",
- "django.contrib.contenttypes",
- "django.contrib.sessions",
- "django.contrib.messages",
- "django.contrib.staticfiles",
- "pages", # new
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "pages", #new 
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS":  [BASE_DIR / "templates"], # new
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

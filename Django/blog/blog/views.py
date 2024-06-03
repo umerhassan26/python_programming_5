a34e79cb59b79574842097b1b3464156bb1da74a
umerhassan26 <umerhassan2694@gmail.com>
2024-06-03 16:40:10 +0500

    Forms Create a View

--- Django/blog/blog/views.py
+++ Django/blog/blog/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView # new
 from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
 class BlogDetailView(DetailView): # new
- model = Post
- template_name = "post_detail.html"     
+    model = Post
+    template_name = "post_detail.html"  
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]   
